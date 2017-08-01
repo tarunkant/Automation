8c4dc34ea1f34a6d999decb4d14048bcb300100c
tarunkant <tarunkant05@gmail.com>
2017-08-01 20:38:13 +0530

    Download Files

--- FileDownloader.py
+++ FileDownloader.py
@@ -5,11 +5,16 @@ from BeautifulSoup import BeautifulSoup
 import os
 import sys
 
-url=sys.argv[1]
-
-html_page = urllib2.urlopen(url)
-soup = BeautifulSoup(html_page)
-for link in soup.findAll('a'):
-	if(".java" in link.get('href')):
-    		os.system("wget " +  url + link.get('href'))
+try:
+	url=sys.argv[1]
+except IndexError:
+	print "Give url as command arguement"
+try:
+	html_page = urllib2.urlopen(url)
+	soup = BeautifulSoup(html_page)
+	for link in soup.findAll('a'):
+		if(".java" in link.get('href')):
+    			os.system("wget " +  url + link.get('href'))
     		
+except NameError:
+	print "Unable to read the file"
